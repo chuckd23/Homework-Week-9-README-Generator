27446e0d09386a060485ce079fbb1c16a921ff60
Kalyn Reid <reidkalyn361@gmail.com>
2021-07-11 21:22:18 -0700

    Placed data in the license functions and tied the init to the writeToFile and generateMarkdown functions.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function userInput(){
     {name: 'license',
     message: 'Select kind of license for this application:',
     type: 'list',
-    choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', "BSD 3-clause Clear license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL License", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]},
+    choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause license", "BSD 3-clause license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]},
     {name: 'title',
     message: 'What is the title of this application?',
     type: 'input'},
@@ -35,6 +35,9 @@ function userInput(){
     {name: 'email',
     message: 'What is your e-mail?',
     type: 'input'},
+    {name: 'fileName',
+    message: 'What do you want to call this readme?',
+    type: 'input'},
 ])};
 
 // TODO: Create a function to write README file
@@ -46,7 +49,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 async function init() {
     let answers = await userInput();
-    console.log(answers);
+    writeToFile((answers.fileName),(generateMarkdown(answers)));
 }
 
 // Function call to initialize app
