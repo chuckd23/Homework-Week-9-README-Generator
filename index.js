ee60556978eb7cf6f6d9838fd728f5f74b582b40
Kalyn Reid <reidkalyn361@gmail.com>
2021-07-11 19:29:23 -0700

    Added markdown placeholder and a place for the answers to go.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const questions = [
     {name: 'license',
     message: 'Select kind of license for this application:',
     type: 'list',
-    choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', "BSD 3-clause Clear license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL License", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]},
+    choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", 'BSD 2-clause "Simplified" license', 'BSD 3-clause "New" or "Revised" license', "BSD 3-clause Clear license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0",  "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License",  "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "PostgreSQL License", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]},
     {name: 'title',
     message: 'What is the title of this application?',
     type: 'input'},
@@ -43,33 +43,35 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 async function init() {
+    let answers = []
     await inquirer
         .prompt(questions[0])
-        .then(answer => {console.log(answer.license)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[1])
-        .then(answer => {console.log(answer.title)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[2])
-        .then(answer => {console.log(answer.description)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[3])
-        .then(answer => {console.log(answer.install)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[4])
-        .then(answer => {console.log(answer.usage)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[5])
-        .then(answer => {console.log(answer.contributing)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[6])
-        .then(answer => {console.log(answer.testing)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[7])
-        .then(answer => {console.log(answer.username)})
+        .then(answer => {answers.push(answer)})
     await inquirer
         .prompt(questions[8])
-        .then(answer => {console.log(answer.email)})
+        .then(answer => {answers.push(answer)})
+    const markdownText = await generateMarkdown({...answers});
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,43 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${{ title }}
+  ## Badges
+  ${placeholder}
 
-`;
+  ## Table of Contents
+  - [License](#license)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [How to Contribute](#how-to-contribute)
+  - [Tests](#tests)
+  - [Questions?](#questions)
+
+  ## License
+  ${licenseLinkPlaceholder}
+  ${licenseTextPlaceholder}
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## How to Contribute
+  [Contributor Covenant](https://www.contributor-covenant.org/)
+  ${data.contributing}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions?
+  ### Reach me here: 
+  https://github.com/${data.username}
+  ${data.email}`;
 }
 
 module.exports = generateMarkdown;
